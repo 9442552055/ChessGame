04d2a34402723d9e0705df2d1731c0936804b2b5
arunmozhi <arunmozhi@live.in>
2017-08-27 02:10:42 +0530

    Tower move predictions done!

--- src/app/ChessCoins/Tower.ts
+++ src/app/ChessCoins/Tower.ts
@@ -10,15 +10,36 @@ import { ChesscoinService } from "./ChesscoinService"; import { ChessboardServic
 })
 export class Tower extends AbstractChessCoin {
     Name: String = "tower";
-    constructor(ChesscoinService: ChesscoinService,@Inject('ICoinShiftable') CoinShifter: ChessboardService) {
+    constructor(ChesscoinService: ChesscoinService, @Inject('ICoinShiftable') CoinShifter: ChessboardService) {
         //super( "tower", _color == ChessColor.black ? "&#9820;" : "&#9814;");
-        super(ChesscoinService,CoinShifter);
+        super(ChesscoinService, CoinShifter);
     }
     GetShiftablePlaces(): ChessPosition[] {
         //Full column front and back upto any coin/end
         //Full row left and right upto any coin/end
         //and moving this should not lead check to king
-        return [];
+        var places = [];
+        var addPositions = function (ChesscoinService, MyColor) {
+            return function (p: ChessPosition, type: string, val: number) {
+                if ((p[type] < 7 && val == 1) || (val == -1 && p[type] > 0)) {
+                    var newP = { ...p };
+                    newP[type] = newP[type] + val;
+                    var coinInPath = ChesscoinService.GetCoinIfExists(newP);
+                    if (!coinInPath) {
+                        places.push(newP);
+                        addPositions(newP, type, val);
+                    }
+                    else if (coinInPath.Color != MyColor) {
+                        places.push(newP);
+                    }
+                }
+            }
+        }(this.__chesscoinService, this.Color);
+        addPositions(this.ChessCellPosition, "Row", 1)
+        addPositions(this.ChessCellPosition, "Row", -1)
+        addPositions(this.ChessCellPosition, "Column", 1)
+        addPositions(this.ChessCellPosition, "Column", -1)
+        return places;
     }
 }
 export default Tower;
